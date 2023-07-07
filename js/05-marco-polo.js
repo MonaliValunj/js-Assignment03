e1bfc0dd4c5b151582087e02e5a0011d07683b22
Monali Valunj <monavalunj96@gmail.com>
2023-07-07 15:30:13 -0700

    Marco! Polo ! completed

--- js/05-marco-polo.js
+++ js/05-marco-polo.js
@@ -3,13 +3,15 @@
 // Marco!!Polo
 
 for(let i = 1; i<=100; i++){
-    if(i % 3 == 0){
-        console.log(i +" Marco!")
+    if(i % 3 == 0 && i % 5 == 0){ 
+       // console.log(i +"  Marco! Polo!!")
+        console.log("Marco! Polo!!")
     }else if(i % 5 == 0){
-        console.log(i+ " Polo!")
-    }else if(i % 3 == 0 && i % 5 == 0){
-        console.log(i +" Marco! Polo!")
-    }else{
-        console.log("nothing")
+       // console.log(i+ " Polo!")
+        console.log( " Polo!")
+    }else  if(i % 3 == 0) {
+        //console.log(i +" marco ")
+        console.log(" marco ")
     }
 }
+// 
